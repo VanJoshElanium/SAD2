8d6c4dd6c1e3227c07bb1e81f9e3611b673dbdca
Ann-Louise <annlouisegaynor@gmail.com>
2017-11-29 03:16:45 +0800

    Edit login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,61 +1,62 @@
-@extends('layouts.app-login')
-
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+<html>
+    @extends('layouts.app-login')
+    @section('content')
+    <div class="container">
+        <div class="flexBox vertical-center" style="display: flex; flex-flow: row wrap; justify-content: center;">
             <div class="panel panel-default">
-                <div class="panel-body">
+                <div class="panel-body text-center">
+                    
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
-                         <img src="/images/PP.png" class="pp-logo">
+                        <img src="/images/pp-logo-white.png" class="pp-logo"> <!-- P&P LOGO -->
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                           
-                            <div class="col-md-6">
-                                <img src= "/images/user.png" class="email-tb">
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}"> 
+                            <!-- USER ICON-->
+                            <img src= "/images/user.png" class="email-tb"> 
 
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus placeholder="E-mail Address">
+                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus placeholder="Username">
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <div class="col-md-6">
-                                <img src= "/images/lock.png" class="lock-tb">
+                            <!-- PASSWORD ICON-->
+                            <img src= "/images/lock.png" class="lock-tb">
 
-                                <input id="password" type="password" class="form-control" name="password" required placeholder="Password">
+                            <input id="password" type="password" class="form-control" name="password" required placeholder="Password">
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                    <button type="submit" class="btn btn-primary">
-                                    Login
+                        <div class="col-lg-9 col-lg-offset-1">
+                            <div class="form-group text-center">
+                                <button type="submit" class="btn btn-primary btn-block"> 
+                                    Login 
                                 </button>
-                                </div>
                             </div>
                         </div>
+
+                        <!--
+                        <div class=" checkbox">
+                            <label>
+                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                            </label>
+                        </div> 
+                        -->
+
                     </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    @endsection
+</html>
