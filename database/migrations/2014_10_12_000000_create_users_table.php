d581f31bae56af601b18d268275a925dde433f0b
Ann-Louise <annlouisegaynor@gmail.com>
2017-11-28 23:34:49 +0800

    Update 2014_10_12_000000_create_users_table.php
    Had the wrong code before.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,16 +1,35 @@
-> Project contains 
-  : An edited login page
-  - Unsure how to adjust the width of the container of the logo, username and password textbox
-  : Light Dashboard Theme (\public\light-dashboard)
-   - Not yet connected to the home page
+<?php
 
-> How to View/Edit Project
-1.) Extract the file to your preferred destination.
-2.) Open the command prompt.
-3.) Change directory to the path of the project (cd \path-of-project).
-4.) Type in 'php artisan serve' (Take note of the port number it outputs)
-5.) Create a database in PhpMyAdmin called 'laravel'.
-6.) In the command promt, type in 'php artisan migrate'.
-7.) Go back to PhpMyAdmin and create a user in the 'users' table.
-8.) Go to your browser and type in 'localhost:8080/register' (change 8080 to the port number that Laravel gave you).
-9.) Login with your created user's credentials.
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
