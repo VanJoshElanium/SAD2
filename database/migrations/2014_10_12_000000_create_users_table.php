ca8f07c3fbcfe929dfd8b0f6511a4de11cbfa681
Ann-Louise <annlouisegaynor@gmail.com>
2017-12-01 20:51:11 +0800

    Add files via upload

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Carbon\Carbon;
 
 class CreateUsersTable extends Migration
 {
@@ -22,12 +23,33 @@ class CreateUsersTable extends Migration
             $table->string('password', '255');
             $table->integer('gender');
             $table->date('bday', '6');
-            $table->integer('cnum');
+            $table->string('cnum', '11');
             $table->integer('user_type');
             $table->integer('user_status');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        $password = bcrypt('admin123');
+        $current_time = Carbon::now()->toDateTimeString();
+
+        DB::table('users')->insert(
+        array(
+            'fname' => 'Admin',
+            'mname' => 'A',
+            'lname' => 'Admin', 
+            'username' => 'admin123',
+            'password' => $password,
+            'gender' => 1,
+            'bday' => '2017-01-12',
+            'cnum' => '12345678900',
+            'user_type' => 0,
+            'user_status' => 1,
+            'created_at' => $current_time,
+            'updated_at' => $current_time
+        )
+ 
+    );
     }
 
     /**
