2e3df1b4ae649e8615ad1b015a883338894353ac
Ann-Louise <annlouisegaynor@gmail.com>
2017-12-10 19:26:09 +0800

    Add files via upload
    new tables
    -suppliers
    -supplies (w/ foreign key linked to suppliers)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,7 +25,7 @@ class CreateUsersTable extends Migration
             $table->date('bday', '6');
             $table->string('cnum', '11');
             $table->integer('user_type');
-            $table->integer('user_status');
+            $table->integer('user_status')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2017_12_09_092403_create_suppliers_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class 20141012200000CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->increments('supplier_id');
+            $table->string('supplier_name', '50')->unique();
+            $table->string('supplier_addr', '70');
+            $table->string('supplier_email', '50')
+            $table->integer('supplier_cnum', '11');        
+            $table->integer('supplier_status')->default(1);
+            $table->rememberToken();
+            $table->timestamps();
+
+        Schema::create('supplies', function (Blueprint $table) {
+            $table->increments('supply_id');
+            $table->integer('supply_supplier_id');
+            $table->string('supply_name', '50');
+            $table->integer('supply_price', '10');
+            $table->ineteger('supply_status')->default(1);
+            $table->foreign('supply_supplier_id')
+                    ->references('supplier_id')
+                    ->on('suppliers')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+            $table->rememberToken();
+            $table->timestamps();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
