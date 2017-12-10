61f659513b3ac2a123884da1f780b81d2bdfc3fd
VanJoshElanium <33405227+VanJoshElanium@users.noreply.github.com>
2017-12-10 23:01:00 +0800

    Update usrmgmt.blade.php

--- resources/views/usrmgmt.blade.php
+++ resources/views/usrmgmt.blade.php
@@ -56,14 +56,14 @@
                         </a>
                     </li>
                     <li>
-                        <a href="{{ route('usrprofile') }}"
+                        <a href="/public/html/user.html"
                             <i class="pe-7s-user"></i>
                             <p>User Profile</p>
                         </a>
                     </li>
 
                     <li>
-                        <a href="{{ route('term') }}">
+                        <a href="/public/html/icons.html">
                             <i class="pe-7s-rocket"></i>
                             <p>Terms</p>
                         </a>
@@ -81,7 +81,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="{{ route('logs') }}">
+                        <a href="/public/html/template.html">
                             <i class="pe-7s-folder"></i>
                             <p>Logs</p>
                         </a>
