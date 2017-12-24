2225e358d60e6ffd39b59d7c3a11699ce3e30246
Ann-Louise <annlouisegaynor@gmail.com>
2017-12-24 22:17:51 +0800

    Newly Updated

--- app/Inventory.php
+++ app/Inventory.php
@@ -2,9 +2,39 @@
 
 namespace App;
 
+use Laravel\Scout\Searchable;
+use Kyslik\ColumnSortable\Sortable;
 use Illuminate\Database\Eloquent\Model;
 
 class Inventory extends Model
 {
-    //
+    use Searchable;
+    use Sortable;
+
+    protected $primaryKey = 'inventory_id';
+
+    protected $fillable = [
+        'inventory_quantity', 'inventory_price', 'inventory_status', 'inventory_damaged', 'received_at', 'inventory_user_id',
+    ];
+
+    public $sortable = ['inventory_quantity', 'inventory_id', 'inventory_price', 'received_at'];
+
+    protected $hidden = [
+        'remember_token',
+    ];
+
+    /* MUTATORS */
+
+    /* ACCESSORS */
+
+    /* OTHERS */
+    public function searchableAs()
+    {
+        return 'inventory_index';
+    }
+
+    public function inventory()
+    {
+        return $this->hasMany('App\Inventory');
+    }
 }
