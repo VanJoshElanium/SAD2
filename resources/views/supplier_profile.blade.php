39ae20bfae80d7b1cecdc973c61becde0e009610
VanJoshElanium <33405227+VanJoshElanium@users.noreply.github.com>
2018-01-09 20:08:14 +0800

    Add files via upload

--- resources/views/supplier_profile.blade.php
+++ resources/views/supplier_profile.blade.php
@@ -105,7 +105,7 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="#">Supplier Profile</a>
+                        <a class="navbar-brand" href="#">Supplier Profile </a>
                    </div>
                     <div class="collapse navbar-collapse">
                         <ul class="nav navbar-nav navbar-right">
