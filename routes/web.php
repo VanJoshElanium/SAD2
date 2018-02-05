dc5eb5bc8f0130babdd493e67f135dd2f50cab0c
Ann-Louise <annlouisegaynor@gmail.com>
2018-02-06 03:01:56 +0800

    Updated web.php
    New routes for new models & controllers

--- routes/web.php
+++ routes/web.php
@@ -1,64 +1,86 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+	/*
+	|--------------------------------------------------------------------------
+	| Web Routes
+	|--------------------------------------------------------------------------
+	|
+	| Here is where you can register web routes for your application. These
+	| routes are loaded by the RouteServiceProvider within a group which
+	| contains the "web" middleware group. Now create something great!
+	|
+	*/
 
-Auth::routes();
+	Auth::routes();
 
-Route::get('/', function () {return view('auth.login');});
+	Route::get('/', function () {return view('auth.login');});
 
-Route::get('/home', 'DashboardController@index')->name('dashboard');
-Route::get('/profile', 'ProfileController@index')->name('profile');
+	Route::get('/home', 'DashboardController@index')->name('dashboard');
+	Route::get('/profile', 'ProfileController@index')->name('profile');
 
-//SUPPLIER MANAGEMENT
-Route::resource('suppliers', 'SupplierController');
-Route::get('/suppliers', 'SupplierController@index')->name('suppliers');
-Route::get('getSupplier/{id}', 'SupplierController@getSupplier');
-Route::get('/searchSuppliers', ['as'=>'searchSuppliers','uses'=>'SupplierController@index']);
+	//SUPPLIER MANAGEMENT
+	Route::resource('suppliers', 'SupplierController');
+	Route::get('/suppliers', 'SupplierController@index')->name('suppliers');
+	Route::get('getSupplier/{id}', 'SupplierController@getSupplier');
+	Route::get('/searchSuppliers', ['as'=>'searchSuppliers','uses'=>'SupplierController@index']);
 
-//SUPPLY MANAGEMENT
-Route::resource('supplies', 'SupplyController');
-Route::get('supplies/{id}', 'SupplyController@show');
-Route::get('getSuppliedItem/{id}', 'SupplyController@getSuppliedItem');
-Route::get('/searchSupplies', ['as'=>'searchSupplies','uses'=>'SupplyController@index']);
+	//SUPPLY MANAGEMENT
+	Route::resource('supplies', 'SupplyController');
+	Route::get('getSuppliedItem/{id}', 'SupplyController@getSuppliedItem');
+	Route::get('/searchSupplies', ['as'=>'searchSupplies','uses'=>'SupplyController@index']);
 
-//USER MANAGEMENT
-Route::resource('create_users', 'UserController');
-Route::get('/usrmgmt', 'UserController@index')->name('usrmgmt');
-Route::get('getUser/{id}', 'UserController@getUser');
-Route::get('/searchUsers', ['as'=>'searchUsers','uses'=>'UserController@index']);
-Route::post('changePassword/{id}','UserController@changePassword');
+	//USER MANAGEMENT
+	Route::resource('create_users', 'UserController');
+	Route::get('/usrmgmt', 'UserController@index')->name('usrmgmt');
+	Route::get('getUser/{id}', 'UserController@getUser');
+	Route::get('/searchUsers', ['as'=>'searchUsers','uses'=>'UserController@index']);
+	Route::post('changePassword/{id}','UserController@changePassword');
 
 
-//INVENTORY
-Route::resource('inventory', 'InventoryController');
-Route::get('getItem/{id}', 'InventoryController@getItem');
-Route::get('getSupply/{id}', 'InventoryController@getSupply');
-Route::get('/searchItems', ['as'=>'searchItems','uses'=>'InventoryController@index']);
-Route::get('/inventory', 'InventoryController@index')->name('inventory');
+	//INVENTORY
+	Route::resource('inventory', 'InventoryController');
+	Route::get('getItem/{id}', 'InventoryController@getItem');
+	Route::get('getSupply/{id}', 'InventoryController@getSupply');
+	Route::get('/searchItems', ['as'=>'searchItems','uses'=>'InventoryController@index']);
+	Route::get('/inventory', 'InventoryController@index')->name('inventory');
 
-//TERM MANAGEMENT
-Route::resource('terms', 'TermsController');
-Route::get('/terms', 'TermsController@index')->name('terms');
+	//REPAIRS
+	Route::resource('repair', 'RepairController');
+	Route::get('getRepair/{id}', 'RepairController@getRepair');
 
-//T-PROFILE
-Route::resource('termsprofile', 'TermsProfileController');
-Route::get('/termsprofile', 'TermsProfileController@index')->name('termsprofile');
+	//TERM MANAGEMENT
+	Route::resource('terms', 'TermsController');
+	Route::get('/terms', 'TermsController@index')->name('terms');
 
-//T-WORKERS
-Route::resource('workers', 'WorkerController');
+	//T-PROFILE
+	Route::resource('termsprofile', 'TermsProfileController');
+	Route::get('/termsprofile', 'TermsProfileController@index')->name('termsprofile');
 
-//LOGS 
-Route::resource('logs', 'LogsController');
-Route::get('/logs', 'LogsController@index')->name('logs');
+	//T-WORKERS
+	Route::resource('workers', 'WorkerController');
+	Route::get('getWorker/{id}', 'WorkerController@getWorker');
 
+	//T-EXPENSES
+	Route::resource('expenses', 'ExpenseController');
+	Route::get('getExpense/{id}', 'ExpenseController@getExpense');
+
+	//T-SALES
+	Route::resource('sales', 'SaleController');
+
+	//T-ITEMS
+	Route::resource('term_items', 'Term_ItemController');
+	Route::get('getTermItem/{id}', 'Term_ItemController@getTermItem');
+
+	//T-CUSTOMERS, ORDERS, CUSTOMER_ORDER
+	Route::resource('customers', 'CustomerController');
+	Route::resource('orders', 'OrderController');
+	Route::resource('customer_orders', 'Customer_OrderController');
+
+	Route::get('getCustomerOrder', 'CustomerController@getCustomerOrder');
+
+	
+	//LOGS 
+	Route::resource('logs', 'LogsController');
+	Route::get('/logs', 'LogsController@index')->name('logs');
 
 ?>
