32fadee5191355b57a51c909008919df0b78509d
Ann-Louise <annlouisegaynor@gmail.com>
2018-02-14 09:00:14 +0800

    Updated migrations

--- database/migrations/2018_01_25_155144_create_repairs_table.php
+++ database/migrations/2018_01_25_155144_create_repairs_table.php
@@ -17,25 +17,9 @@ class CreateRepairsTable extends Migration
         Schema::create('repairs', function (Blueprint $table) {
             $table ->increments('repair_id');
             $table ->integer('repair_inventory_id') -> unsigned();
-            $table ->integer('repair_term_id') -> nullable() -> unsigned();
-            $table ->integer('repair_user_ud') -> nullable() -> unsigned();
             
-            $table ->dateTime('repair_date') -> default(Carbon::now());
             $table ->integer('repair_qty');
-            $table ->string('repair_desc') -> nullable();
             $table ->integer('repair_status') -> default(0);
-
-            $table ->foreign('repair_inventory_id')
-                    ->references('inventory_id')
-                    ->on('inventories')
-                    ->onUpdate('cascade')
-                    ->onDelete('cascade');
-
-            $table ->foreign('repair_term_id')
-                    ->references('term_id')
-                    ->on('terms')
-                    ->onUpdate('cascade')
-                    ->onDelete('cascade');
             $table ->timestamps();
         });
     }
