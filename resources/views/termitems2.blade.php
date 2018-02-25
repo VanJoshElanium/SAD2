7e28e52cd01aaf13ae1fce9267bf9c5129703550
Ann-Louise <annlouisegaynor@gmail.com>
2018-02-25 10:09:41 +0800

    Create termitems2.blade.php

---
+++ resources/views/termitems2.blade.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD html 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=8">
+<title>bcl_873612365.htm</title>
+<meta name="generator" content="BCL easyConverter SDK 5.0.08">
+<style type="text/css">
+
+body {margin-top: 0px;margin-left: 0px;}
+
+#page_1 {position:relative; overflow: hidden;margin: 68px 0px 790px 47px;padding: 0px;border: none;width: 722px;}
+
+
+
+
+
+.ft0{font: bold 11px 'Calibri';line-height: 13px;}
+.ft1{font: 11px 'Calibri';line-height: 13px;}
+.ft2{font: 1px 'Calibri';line-height: 1px;}
+.ft3{font: 1px 'Calibri';line-height: 7px;}
+.ft4{font: bold 10px 'Calibri';line-height: 13px;}
+.ft5{font: 1px 'Calibri';line-height: 5px;}
+.ft6{font: 10px 'Calibri';line-height: 13px;}
+
+.p0{text-align: left;padding-left: 8px;margin-top: 0px;margin-bottom: 0px;white-space: nowrap;}
+.p1{text-align: left;margin-top: 0px;margin-bottom: 0px;white-space: nowrap;}
+.p2{text-align: left;padding-left: 12px;margin-top: 0px;margin-bottom: 0px;white-space: nowrap;}
+.p3{text-align: center;padding-left: 2px;margin-top: 0px;margin-bottom: 0px;white-space: nowrap;}
+.p4{text-align: center;padding-right: 144px;margin-top: 0px;margin-bottom: 0px;white-space: nowrap;}
+.p5{text-align: left;padding-left: 6px;margin-top: 0px;margin-bottom: 0px;white-space: nowrap;}
+.p6{text-align: center;padding-right: 1px;margin-top: 0px;margin-bottom: 0px;white-space: nowrap;}
+.p7{text-align: right;padding-right: 75px;margin-top: 0px;margin-bottom: 0px;white-space: nowrap;}
+.p8{text-align: left;padding-left: 98px;margin-top: 0px;margin-bottom: 0px;white-space: nowrap;}
+.p9{text-align: right;padding-right: 23px;margin-top: 0px;margin-bottom: 0px;white-space: nowrap;}
+
+.td0{padding: 0px;margin: 0px;width: 186px;vertical-align: bottom;}
+.td1{padding: 0px;margin: 0px;width: 170px;vertical-align: bottom;}
+.td2{padding: 0px;margin: 0px;width: 119px;vertical-align: bottom;}
+.td3{padding: 0px;margin: 0px;width: 49px;vertical-align: bottom;}
+.td4{padding: 0px;margin: 0px;width: 114px;vertical-align: bottom;}
+.td5{padding: 0px;margin: 0px;width: 84px;vertical-align: bottom;}
+.td6{border-bottom: #000000 1px solid;padding: 0px;margin: 0px;width: 44px;vertical-align: bottom;}
+.td7{border-bottom: #000000 1px solid;padding: 0px;margin: 0px;width: 142px;vertical-align: bottom;}
+.td8{border-bottom: #000000 1px solid;padding: 0px;margin: 0px;width: 170px;vertical-align: bottom;}
+.td9{border-bottom: #000000 1px solid;padding: 0px;margin: 0px;width: 119px;vertical-align: bottom;}
+.td10{border-bottom: #000000 1px solid;padding: 0px;margin: 0px;width: 49px;vertical-align: bottom;}
+.td11{border-bottom: #000000 1px solid;padding: 0px;margin: 0px;width: 114px;vertical-align: bottom;}
+.td12{border-bottom: #000000 1px solid;padding: 0px;margin: 0px;width: 84px;vertical-align: bottom;}
+.td13{border-left: #000000 1px solid;border-right: #000000 1px solid;padding: 0px;margin: 0px;width: 42px;vertical-align: bottom;}
+.td14{padding: 0px;margin: 0px;width: 142px;vertical-align: bottom;}
+.td15{border-right: #000000 1px solid;padding: 0px;margin: 0px;width: 113px;vertical-align: bottom;}
+.td16{border-right: #000000 1px solid;padding: 0px;margin: 0px;width: 83px;vertical-align: bottom;}
+.td17{border-left: #000000 1px solid;border-right: #000000 1px solid;border-bottom: #000000 1px solid;padding: 0px;margin: 0px;width: 42px;vertical-align: bottom;}
+.td18{border-right: #000000 1px solid;border-bottom: #000000 1px solid;padding: 0px;margin: 0px;width: 113px;vertical-align: bottom;}
+.td19{border-right: #000000 1px solid;border-bottom: #000000 1px solid;padding: 0px;margin: 0px;width: 83px;vertical-align: bottom;}
+.td20{border-right: #000000 1px solid;padding: 0px;margin: 0px;width: 48px;vertical-align: bottom;}
+.td21{border-right: #000000 1px solid;border-bottom: #000000 1px solid;padding: 0px;margin: 0px;width: 48px;vertical-align: bottom;}
+.td22{border-left: #000000 1px solid;padding: 0px;margin: 0px;width: 43px;vertical-align: bottom;}
+.td23{border-right: #000000 1px solid;padding: 0px;margin: 0px;width: 169px;vertical-align: bottom;}
+.td24{border-left: #000000 1px solid;border-right: #000000 1px solid;border-bottom: #000000 1px solid;padding: 0px;margin: 0px;width: 354px;vertical-align: bottom;}
+.td25{border-right: #000000 1px solid;border-bottom: #000000 1px solid;padding: 0px;margin: 0px;width: 281px;vertical-align: bottom;}
+.td26{padding: 0px;margin: 0px;width: 356px;vertical-align: bottom;}
+.td27{padding: 0px;margin: 0px;width: 282px;vertical-align: bottom;}
+
+.tr0{height: 14px;}
+.tr1{height: 20px;}
+.tr2{height: 7px;}
+.tr3{height: 24px;}
+.tr4{height: 17px;}
+.tr5{height: 5px;}
+.tr6{height: 39px;}
+
+.t0{width: 722px;font: 11px 'Calibri';}
+
+</style>
+</head>
+
+<body>
+<DIV id="page_1">
+
+
+<table cellpadding=0 cellspacing=0 class="t0">
+<tr>
+    <td colspan=2 class="tr0 td0"><p class="p0 ft1"><span class="ft0">Collector: </span>{{$term_items[0]-> cfname}} {{$term_items[0]-> cmname}}. {{$term_items[0]-> clname}}</p></td>
+    <td class="tr0 td1"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr0 td2"><p class="p2 ft0">Term date: <span class="ft1">{{$term_items[0] -> start_date}}</span></p></td>
+    <td class="tr0 td3"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr0 td4"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr0 td5"><p class="p1 ft2">&nbsp;</p></td>
+</tr>
+<tr>
+    <td colspan=2 class="tr1 td0"><p class="p0 ft0">Location: <span class="ft1">{{$term_items[0] -> location}}</span></p></td>
+    <td class="tr1 td1"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr1 td2"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr1 td3"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr1 td4"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr1 td5"><p class="p1 ft2">&nbsp;</p></td>
+</tr>
+<tr>
+    <td class="tr2 td6"><p class="p1 ft3">&nbsp;</p></td>
+    <td class="tr2 td7"><p class="p1 ft3">&nbsp;</p></td>
+    <td class="tr2 td8"><p class="p1 ft3">&nbsp;</p></td>
+    <td class="tr2 td9"><p class="p1 ft3">&nbsp;</p></td>
+    <td class="tr2 td10"><p class="p1 ft3">&nbsp;</p></td>
+    <td class="tr2 td11"><p class="p1 ft3">&nbsp;</p></td>
+    <td class="tr2 td12"><p class="p1 ft3">&nbsp;</p></td>
+</tr>
+<tr>
+    <td class="tr3 td6"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr3 td7"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr3 td8"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr3 td9"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr3 td10"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr3 td11"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr3 td12"><p class="p1 ft2">&nbsp;</p></td>
+</tr>
+@foreach($term_items as $term_item)
+<tr>
+    <td class="tr4 td13"><p class="p3 ft0">No.</p></td>
+    <td class="tr4 td14"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr4 td1"><p class="p4 ft4">Name</p></td>
+    <td class="tr4 td2"><p class="p5 ft0">Price</p></td>
+    <td class="tr4 td3"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr4 td15"><p class="p5 ft0">Quantity</p></td>
+    <td class="tr4 td16"><p class="p6 ft4">Checker</p></td>
+</tr>
+<tr>
+    <td class="tr5 td17"><p class="p1 ft5">&nbsp;</p></td>
+    <td class="tr5 td7"><p class="p1 ft5">&nbsp;</p></td>
+    <td class="tr5 td8"><p class="p1 ft5">&nbsp;</p></td>
+    <td class="tr5 td9"><p class="p1 ft5">&nbsp;</p></td>
+    <td class="tr5 td10"><p class="p1 ft5">&nbsp;</p></td>
+    <td class="tr5 td18"><p class="p1 ft5">&nbsp;</p></td>
+    <td class="tr5 td19"><p class="p1 ft5">&nbsp;</p></td>
+</tr>
+<tr>
+    <td class="tr4 td13"><p class="p3 ft1">1</p></td>
+    <td class="tr4 td14"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr4 td1"><p class="p4 ft6"> {{$term_item -> inventory_name }}</p></td>
+    <td class="tr4 td2"><p class="p7 ft1">&#8369; {{$term_item -> inventory_price + ($term_item -> inventory_price * 0.25)}}</p></td>
+    <td class="tr4 td20"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr4 td15"><p class="p5 ft1">{{$term_item -> ti_original }}</p></td>
+    <td class="tr4 td16"><p class="p6 ft1">_____</p></td>
+</tr>
+@endforeach
+<tr>
+    <td class="tr5 td17"><p class="p1 ft5">&nbsp;</p></td>
+    <td class="tr5 td7"><p class="p1 ft5">&nbsp;</p></td>
+    <td class="tr5 td8"><p class="p1 ft5">&nbsp;</p></td>
+    <td class="tr5 td9"><p class="p1 ft5">&nbsp;</p></td>
+    <td class="tr5 td21"><p class="p1 ft5">&nbsp;</p></td>
+    <td class="tr5 td18"><p class="p1 ft5">&nbsp;</p></td>
+    <td class="tr5 td19"><p class="p1 ft5">&nbsp;</p></td>
+</tr>
+<tr>
+    <td class="tr4 td22"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr4 td14"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr4 td23"><p class="p8 ft1">Total quantity:</p></td>
+    <td class="tr4 td2"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr4 td3"><p class="p9 ft1">12</p></td>
+    <td class="tr4 td15"><p class="p1 ft2">&nbsp;</p></td>
+    <td class="tr4 td16"><p class="p1 ft2">&nbsp;</p></td>
+</tr>
+<tr>
+    <td colspan=3 class="tr5 td24"><p class="p1 ft5">&nbsp;</p></td>
+    <td colspan=3 class="tr5 td25"><p class="p1 ft5">&nbsp;</p></td>
+    <td class="tr5 td19"><p class="p1 ft5">&nbsp;</p></td>
+</tr>
+<tr>
+    <td colspan=3 class="tr6 td26"><p class="p0 ft1">Handler/Worker:___________________________</p></td>
+    <td colspan=3 class="tr6 td27"><p class="p2 ft1">Date released:________________________</p></td>
+    <td class="tr6 td5"><p class="p1 ft2">&nbsp;</p></td>
+</tr>
+</table>
+</DIV>
+</body>
+</html>
