82581bd50fabb318db3f2c7886767da3b42d82d4
Ann-Louise <annlouisegaynor@gmail.com>
2018-03-21 18:56:28 +0800

    Add files via upload

--- app/Stockin.php
+++ app/Stockin.php
@@ -3,19 +3,29 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Stockin extends Model
 {
+    use LogsActivity;
+
     protected $primaryKey = 'si_id';
 
     protected $fillable = [
-        'si_date', 'si_inventory_id', 'si_user_id,', 'si_qty'
+        'si_inventory_id', 'si_qty', 'si_si_id', 'si_date'
     ];
 
-    public function inventory()
-    {
-        return $this->belongsTo('App\Inventory', 'si_inventory_id', 'inventory_id');
-    }
+    protected static $logAttributes = [
+        'si_inventory_id', 'si_qty', 'si_si_id', 'si_date'
+    ];
+
+    protected static $logOnlyDirty = true;
+
+    public function getDescriptionForEvent(string $eventName): string
+        {
+            return ucfirst($eventName) . " stock-in";
+        }
+
 
     public function user()
     {
--- app/Term.php
+++ app/Term.php
@@ -3,15 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Term extends Model
 {
+    use LogsActivity;
+
     protected $primaryKey = 'term_id';
 
     /* The attributes that are mass assignable. */
     protected $fillable = [
          'start_date', 'end_date', 'finish_date', 'term_status', 'location'
     ];
+
+
+    protected static $logAttributes = [
+         'start_date', 'end_date', 'finish_date', 'term_status', 'location'
+    ]; 
+
+    protected static $logOnlyDirty = true;
+
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return ucfirst($eventName) . " term details";
+    }
 
     /* RELATIONSHIPS */
     public function workers(){
@@ -28,6 +43,16 @@ class Term extends Model
         return $this->hasMany('App\Expense', 'expense_term_id');
     } 
 
+    public function stockins()
+    {
+        return $this->hasMany('App\Stockin_Item', 'si_term_id');
+    } 
+
+    public function repairs()
+    {
+        return $this->hasMany('App\Repair', 'repair_term_id');
+    } 
+
     public function sales()
     {
         return $this->hasMany('App\Sale', 'sale_term_id');
@@ -41,5 +66,10 @@ class Term extends Model
     public function inventories()
     {
         return $this->belongsToMany('App\Inventories', 'term_items', 'term_id', 'inventory_id');
+    } 
+
+    public function term_items()
+    {
+        return $this->hasMany('App\Term_Item', 'ti_term_id');
     } 
 }
